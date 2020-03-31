070f7fe57422e900b45f2f508acb5f0b91352b4a
Alejaffo <30817476+Alejaffo@users.noreply.github.com>
2020-03-31 19:30:59 -0300

    Cambié mi nombre Alejandro

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Repo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Alejandro");
         }
     }
 }
